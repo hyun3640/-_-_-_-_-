42fad67cf7a59d99f7dfc31ab543d530370a4e2a
hyun3640 <uranus165@gmail.com>
2023-03-09 12:18:16 +0900

    첫 페이지 만들기

---
+++ src/main/java/com/spring/ex01/MainController.java
@@ -0,0 +1,25 @@
+package com.spring.ex01;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("mainController")
+@RequestMapping("/abcderf")
+public class MainController {
+	
+	@RequestMapping(value="/asdqwd.do", method = RequestMethod.GET)
+	public ModelAndView main1(HttpServletRequest request, HttpServletResponse response)  throws Exception{
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("kim","hello23 ");
+		mav.setViewName("kimasd");
+		return mav;
+	}
+	
+	
+	
+}
